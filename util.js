d97cd918b5fe70d86767cbad80f3626a78cd9a24
liyiqiang <1689260771@qq.com>
2021-01-08 13:58:58 +0800

    创建util

---
+++ util.js
@@ -0,0 +1,12 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: happyLee
+ * @Date: 2021-01-08 13:55:13
+ * @LastEditors: happyLee
+ * @LastEditTime: 2021-01-08 13:57:51
+ */
+let a = 12;
+let b = 13;
+[a,b]=[b,a];
+console.log(a,b)
