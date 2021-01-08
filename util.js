c2f3d713c72af59efe2814591ba2ac6aa267fd28
liyiqiang <1689260771@qq.com>
2021-01-08 14:06:17 +0800

    1156修改

--- util.js
+++ util.js
@@ -4,9 +4,10 @@
  * @Author: happyLee
  * @Date: 2021-01-08 13:55:13
  * @LastEditors: happyLee
- * @LastEditTime: 2021-01-08 13:57:51
+ * @LastEditTime: 2021-01-08 14:05:14
  */
 let a = 12;
 let b = 13;
 [a,b]=[b,a];
 console.log(a,b)
+console.log('world')
