10cd5a3989479fddfb5ac40629dc87bc78a00dd5
liyiqiang <1689260771@qq.com>
2021-01-09 04:11:05 +0800

    1135修改

--- util.js
+++ util.js
@@ -4,9 +4,9 @@
  * @Author: happyLee
  * @Date: 2021-01-08 13:55:13
  * @LastEditors: happyLee
- * @LastEditTime: 2021-01-08 14:03:14
+ * @LastEditTime: 2021-01-09 04:07:26
  */
-let a = 12;
+let a = 15;
 let b = 13;
 [a,b]=[b,a];
 console.log(a,b)
